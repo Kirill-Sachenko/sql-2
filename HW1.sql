92c435fc839750a3f27d2b50748496eda1fbf3fb
Kirill <sachenkokirill@yandex.ru>
2024-08-08 18:39:59 +0300

    first commit

---
+++ HW1.sql
@@ -0,0 +1,44 @@
+create table if not exists genre (
+	id serial primary key,
+	genre_name varchar(100) not null unique
+);
+create table if not exists executor (
+	id serial primary key,
+	executor_name varchar(100) not null	
+);
+create table if not exists genre_executor (
+    genre_id integer references genre(id),
+    executor_id integer references executor(id),
+    constraint genre_executor_pk primary key (genre_id, executor_id)
+);
+create table if not exists album (
+	id serial primary key,
+	album_name varchar(100) not null,
+	year_of_release integer not null
+);
+create table if not exists executor_album (
+    executor_id integer references executor(id),
+    album_id integer references album(id),
+    constraint executor_album_pk primary key (executor_id, album_id)
+);
+create table if not exists track (
+	id serial primary key,
+	track_name varchar(100) not null unique,
+	duration integer not null,
+	album_id integer references album(id)
+);
+create table if not exists collection_of_songs (
+    id serial primary key,
+    collection_name varchar(100) not null unique,
+    year_of_release integer not null
+);
+
+CREATE TABLE IF NOT EXISTS track_collection (
+    track_id INTEGER REFERENCES track(id),
+    collection_of_songs_id INTEGER REFERENCES collection_of_songs(id),
+    CONSTRAINT track_collection_pk PRIMARY KEY (track_id, collection_of_songs_id)
+);
+
+
+
+
